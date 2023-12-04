1fa60cf67018d0c2e78f60aa4d22afa9b4ac89e2
eliasib <ingenieroeliasibcadena@gmail.com>
2023-12-04 11:31:16 -0500

    Creación de documentos servomotor camara 1

---
+++ servoMotorCam1.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+#include <servo.h>
+
+servo servoCam1;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  while(){
+  eliacadena}
+}
